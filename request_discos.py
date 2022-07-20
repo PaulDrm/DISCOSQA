d436159a1b4c7808271c2b6e0080e3b824f6b3a4
CallumJWilson <callum.j.wilson@strath.ac.uk>
2022-07-20 13:17:00 +0100

    updating requests functions

--- request_discos.py
+++ request_discos.py
@@ -1,10 +1,21 @@
 """Helper functions for requesting from DISCOSWeb"""
 
 import requests
+from pprint import pprint
+from tqdm import trange
+import time
+import pdb
 
 URL = 'https://discosweb.esoc.esa.int'
 with open('token-discos.txt') as f:
     token = f.read()
+    
+    
+def countdownTimer(Nsec):
+    print(f'Waiting for {Nsec} seconds...')
+    for _ in trange(Nsec):
+        time.sleep(1)
+        
 
 def requestSinglePage(pageName="/api/objects",pageSize=20,filter=None,sort=None,include=None):
     with requests.Session() as s:
@@ -119,6 +130,66 @@ def requestWithRelationships(pageName="/api/objects",pageSize=100,filter=None,so
                     print(f"Retry after: {response.headers['Retry-After']}")
                 return allData
 
+            allData.extend(resp_json['data'])
+            try:
+                incData.extend(resp_json['included'])
+            except KeyError:
+                pass
+            nextLink = resp_json['links']['next']
+
+    return allData, incData
+
+
+def requestAllAndWait(pageName="/api/objects",pageSize=100,filter=None,sort=None,include=None):
+    with requests.Session() as s:
+        response = s.get(
+            URL + pageName,
+            headers={
+                'Authorization': f'Bearer {token}',
+                'DiscosWeb-Api-Version': '2',
+            },
+            params={
+                'page[size]': pageSize,
+                'filter': filter,
+                'sort' : sort,
+                'include' : include,
+            },
+        )
+        resp_json = response.json()
+        if not response.ok:
+            pprint(resp_json['errors'])
+            if resp_json['errors'][0]['status']=='429':
+                print(f"Retry after: {response.headers['Retry-After']}")
+            return ([],[])
+
+        allData = resp_json['data']
+        try:
+            incData = resp_json['included']
+        except KeyError:
+            incData = []
+        nextLink = resp_json['links']['next']
+        totalPages = resp_json['meta']['pagination']['totalPages']
+
+        while nextLink is not None:
+            response = s.get(
+                URL + nextLink,
+                headers={
+                    'Authorization': f'Bearer {token}',
+                    'DiscosWeb-Api-Version': '2',
+                },
+            )
+            resp_json = response.json()
+            if not response.ok:
+                if resp_json['errors'][0]['status']=='429':
+                    print(f'Completed {currentPage} pages of {totalPages}')
+                    t_wait = int(response.headers['Retry-After'])+1
+                    countdownTimer(t_wait)
+                    continue
+                else:
+                    pprint(resp_json['errors'])
+                    return allData, incData
+
+            currentPage = resp_json['meta']['pagination']['currentPage']
             allData.extend(resp_json['data'])
             try:
                 incData.extend(resp_json['included'])

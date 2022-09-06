b0137d371cef67d31ee4fa4b2354f638f339ae30
CallumJWilson <callum.j.wilson@strath.ac.uk>
2022-09-06 20:01:52 +0100

    updating scos file convert

--- convert_scos.py
+++ convert_scos.py
@@ -7,9 +7,11 @@ import pandas as pd
 import pdb
 
 
-def row2rdf_reportingdata(dfRow):
+def row2rdf_reportingdata(dfRow, missionName):
     """Converts data from row of reporting data DataFrame to rdf triples"""
+    
     rdf_str = f"<PCF_{dfRow['PCF_NAME']}> a ioa:ReportingData ;\n"
+    rdf_str += f"  pred:instance_of ioa:ReportingData ;\n"
     rdf_str += f"  ioa:Name '{dfRow['PCF_NAME']}'^^xsd:string ;\n"
     rdf_str += f"  ioa:Description '{dfRow['PCF_DESCR']}'^^xsd:string ;\n"
     
@@ -19,8 +21,37 @@ def row2rdf_reportingdata(dfRow):
     if type(dfRow['PCF_UNIT'])==str:
         rdf_str += f"  ioa:Unit '{dfRow['PCF_UNIT']}'^^xsd:string ;\n"
     
+    #API call string should be edited
+    rdf_str += f"  ioa:APICall '/ares/data?mission={missionName}&parameter={dfRow['PCF_NAME']}&intervalStart={{}}&intervalEnd={{}}'^^xsd:string ;\n"
     rdf_str += f"  pred:name '{dfRow['PCF_NAME']}'^^xsd:string ;\n"
-    rdf_str += f"  ioa:hasParent <{dfRow['parent_name']}> ."
+    rdf_str += f"  ioa:parent <{dfRow['parent_name']}> ."
     
     return rdf_str
+
+def row2rdf_activity(dfRow, missionName):
+    """Converts data from row of activity DataFrame to rdf triples"""
     
+    rdf_str = f"<CCF_{dfRow['CCF_CNAME']}> a ioa:Activity ;\n"
+    rdf_str += f"  pred:instance_of ioa:Activity ;\n"
+    rdf_str += f"  ioa:Name '{dfRow['CCF_CNAME']}'^^xsd:string ;\n"
+    rdf_str += f"  ioa:Description '{dfRow['CCF_DESCR']}'^^xsd:string ;\n"
+    
+    if type(dfRow['CCF_DESCR2'])==str:
+        rdf_str += f"  ioa:DetailedDescription '{dfRow['CCF_DESCR2']}'^^xsd:string ;\n"
+    
+    rdf_str += f"  pred:name '{dfRow['CCF_CNAME']}'^^xsd:string ;\n"
+    rdf_str += f"  ioa:parent <{dfRow['parent_name']}> ."
+    
+    return rdf_str
+
+def row2rdf_event(dfRow, missionName):
+    """Converts data from row of event DataFrame to rdf triples"""
+    
+    rdf_str = f"<PID_{dfRow.name}> a ioa:Event ;\n"
+    rdf_str += f"  pred:instance_of ioa:Event ;\n"
+    rdf_str += f"  ioa:Description '{dfRow['PID_DESCR']}'^^xsd:string ;\n"
+
+    rdf_str += f"  ioa:APICall '/uberlog/entries?mission={missionName}&eventDateFrom={{}}&eventDateTo={{}}&text={dfRow['PID_DESCR'].replace(' ','%20')}'^^xsd:string ;\n"
+    rdf_str += f"  ioa:parent <{dfRow['parent_name']}> ."
+    
+    return rdf_str
